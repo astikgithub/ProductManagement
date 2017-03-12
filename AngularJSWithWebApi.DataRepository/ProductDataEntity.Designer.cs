d2abe4f37933b1b9816be962e7dfe369d1e02055
astikgithub <vachhani.astik@gmail.com>
2017-03-12 21:07:11 +0530

    Change .Net Framework

--- AngularJSWithWebApi.DataRepository/ProductDataEntity.Designer.cs
+++ AngularJSWithWebApi.DataRepository/ProductDataEntity.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'D:\AngularJSTraining\AngularJsApplication\AngularJSWithWebApi\AngularJSWithWebApi.DataRepository\ProductDataEntity.edmx'. 
+﻿// T4 code generation is enabled for model 'F:\AngularJS\AngularJSWithWebApi.DataRepository\ProductDataEntity.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
